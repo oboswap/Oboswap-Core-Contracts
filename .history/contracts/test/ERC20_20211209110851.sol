5a9ddec3bd39adca33fbee54c4f9d02c405f0a7e
Pc-3 <gopalgurram1@gmail.com>
2021-12-22 13:51:42 +0530

    first commit

--- .history/contracts/test/ERC20_20211209110851.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../OboswapERC20.sol';
-
-contract ERC20 is OboswapERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
